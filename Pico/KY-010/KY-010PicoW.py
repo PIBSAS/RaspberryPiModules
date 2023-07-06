b7d09946c7c63768314ad46f302c1a6522b4ab45
Raspberry Pi Buenos Aires <38385411+Luciano2018@users.noreply.github.com>
2023-07-05 22:06:19 -0300

    Create KY-010PicoW.py

---
+++ Pico/KY-010/KY-010PicoW.py
@@ -0,0 +1,16 @@
+from machine import Pin
+import time
+
+led = Pin(LED, Pin.OUT)
+sensor = Pin(22, Pin.IN)
+
+while True:
+    if sensor.value() == 1:
+        led.on()
+        print("ON")
+        print(sensor.value())
+    else:
+        led.off()
+        print("OFF")
+        print(sensor.value())
+#End
