2411e1b58e800532f5a9ea35ff8d82abc629981f
Raspberry Pi Buenos Aires <38385411+Luciano2018@users.noreply.github.com>
2023-07-05 21:58:58 -0300

    Update KY_005.py

--- Pico/KY-005/KY_005.py
+++ Pico/KY-005/KY_005.py
@@ -1,3 +1,5 @@
+#Requiere libraries micropython_ir y micropython-async
+#https://sites.google.com/view/raspberrypibuenosaires/sensores-arduino-y-raspberry-pi/sensores-arduino-y-raspberry-pi-5
 from machine import Pin
 from ir_tx.nec import NEC
 import time
