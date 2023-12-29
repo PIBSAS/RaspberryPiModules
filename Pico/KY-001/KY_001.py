2e936570cc35cc1480ab85b1e63f0e029f547862
Raspberry Pi Buenos Aires <38385411+PIBSAS@users.noreply.github.com>
2023-12-29 13:40:48 -0300

    Update KY_001_Pico.py

--- Pico/KY-001/KY_001.py
+++ Pico/KY-001/KY_001.py
@@ -1,18 +1,19 @@
-import machine, onewire, ds18x20, time
- 
-ds_pin = machine.Pin(22)
- 
-ds_sensor = ds18x20.DS18X20(onewire.OneWire(ds_pin))
- 
+from onewire import Onewire
+from ds18x20 import DS18x20
+from machine import Pin
+from time import sleep, sleep_ms
+
+ds_pin = Pin(22)
+ds_sensor = DS18x20(Onewire(ds_pin))
 roms = ds_sensor.scan()
- 
+
 print('Found DS devices: ', roms)
- 
+
 while True:
     ds_sensor.convert_temp()
-    time.sleep_ms(750)
+    sleep_ms(750)
     for rom in roms:
         print(rom)
         print(ds_sensor.read_temp(rom))
-    time.sleep(5)
+    sleep(5)
 #End
