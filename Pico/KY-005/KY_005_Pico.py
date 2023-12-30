1802ed35ff44f3381a77a23d6c22111c40681274
Raspberry Pi Buenos Aires <38385411+PIBSAS@users.noreply.github.com>
2023-12-29 21:42:21 -0300

    Update KY_005_Pico.py

--- Pico/KY-005/KY_005_Pico.py
+++ Pico/KY-005/KY_005_Pico.py
@@ -2,11 +2,10 @@
 #https://sites.google.com/view/raspberrypibuenosaires/sensores-arduino-y-raspberry-pi/sensores-arduino-y-raspberry-pi-5
 from machine import Pin
 from ir_tx.nec import NEC
-import time
-
+from time import sleep
 nec = NEC(Pin(17, Pin.OUT, value = 0))
 
 while True:
     nec.transmit(1, 2)  # address == 1, data == 2
-    time.sleep(2)
+    sleep(2)
 #End
